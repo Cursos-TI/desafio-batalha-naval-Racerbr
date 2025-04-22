a8fec17ff9997254cc0f6331af8042e25ac405ff
Racerbr <thiagovitoria120@gmail.com>
2025-04-22 11:48:42 -0300

    Update batalhaNaval.c
    Nível Aventureiro (coordenadas na diagonal)

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -2,50 +2,7 @@
 
 
 int main() {
-    
-    char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-    int coluna[10] =   
-        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    
-    
-    
-    printf(" TABULEIRO BATALHA NAVAL\n");
-    printf("  ");
-   
-    for (int j = 0; j < 10; j++){
-        printf("%c ", linha[j]); // Imprime os números das colunas
-    }
-    
-        
-    
-        int tabuleiro[10][10] = {
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-    };
-   
-    for (int i = 0; i < 10; i++){
-        printf("\n%d", coluna[i]);
-        printf(" %d", tabuleiro[i][i]);
-        printf(" %d", tabuleiro[i][i]);
-        printf(" %d", tabuleiro[i][i]);
-        printf(" %d", tabuleiro[i][i]);
-        printf(" %d", tabuleiro[i][i]);
-        printf(" %d", tabuleiro[i][i]);
-        printf(" %d", tabuleiro[i][i]);
-        printf(" %d", tabuleiro[i][i]);
-        printf(" %d", tabuleiro[i][i]);
-        printf(" %d", tabuleiro[i][i]);
-    }
-
-    
+ 
     int escolha;
     
     printf("\n");
@@ -57,10 +14,6 @@ int main() {
     switch (escolha){
         case 1:
             char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-            int coluna[10] =   
-            {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    
-    
     
             printf(" TABULEIRO BATALHA NAVAL\n");
             printf("  ");
@@ -68,41 +21,39 @@ int main() {
             for (int j = 0; j < 10; j++){
                 printf("%c ", linha[j]); // Imprime os números das colunas
             }
+
+            printf("\n");
     
-        
+            int tabuleiro[11][11] = 
+            {{1, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0},
+            {2, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0},
+            {3, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0},
+            {4, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0},
+            {5, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0},
+            {6, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0},
+            {7, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0},
+            {8, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0},
+            {9, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0},
+            {10, 0, 0, 0, 3, 3, 3, 0, 0, 0, 0}};
     
-            int tabuleiro[10][10] = {
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 3, 3, 3, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-            };
-   
+            for (int j = 0; j < 10; j++){
             for (int i = 0; i < 10; i++){
-                printf("\n%d", coluna[i]);
-                printf(" %d", tabuleiro[i][i]);
-                printf(" %d", tabuleiro[i][i]);
-                printf(" %d", tabuleiro[i][4]);
-                printf(" %d", tabuleiro[i][5]);
-                printf(" %d", tabuleiro[i][6]);
-                printf(" %d", tabuleiro[i][i]);
-                printf(" %d", tabuleiro[0][i]);
-                printf(" %d", tabuleiro[1][i]);
-                printf(" %d", tabuleiro[2][i]);
-                printf(" %d", tabuleiro[i][i]);
+           printf("%d ", tabuleiro[j][i]);
         }
+        printf("\n");
+        }    
         break;
+        
         case 2:
+        
         printf("### Saindo... ###\n");
+        
         break;
+        
         default:
+        
         printf("### Opção inválida! Tente novamente ###\n");
+        
         break;
     }
 
